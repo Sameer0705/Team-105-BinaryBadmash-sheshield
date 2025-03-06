3039f282b1284e263e0b887131efbf4c514dbc12
yashdeep singh <124393750+yashdeepsingh2006@users.noreply.github.com>
2025-03-06 10:51:24 +0530

    Update Nav.js

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -49,7 +49,7 @@ export default function Nav() {
                                     Dashboard
                                 </Link>
                                 <Link href="/routes/mint" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
-                                    Dashboard
+                                    New report
                                 </Link>
                             </div>
                         )}
